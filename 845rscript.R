e2ab33e473d4f63662ce59398a02a5f7663b665f
Andrew <vandenhoeven.andrew@gmail.com>
2016-12-13 10:27:08 -0500

    Initial commit

---
+++ 845rscript.R
@@ -0,0 +1,12 @@
+#    845 Project
+#
+#   Leiguang Chen
+# Andrew van den Hoeven
+
+
+
+# begin by clearing our workspace
+rm(list=ls())
+
+#all additional files are assumed to be in the smae directory as the current r script
+setwd(dirname(sys.frame(1)$ofile))
