80463d55698b77330bfa8e8d7b5f62fe1be6c195
Andrew <vandenhoeven.andrew@gmail.com>
2016-12-15 12:28:00 -0500

    lol kk finally figured things out

--- 845Ranalysis.R
+++ 845Ranalysis.R
@@ -1,73 +1,80 @@
-# 845  project 
+#    845 Project
 #
 #   Leiguang Chen
 # Andrew van den Hoeven
-# summary stats
 
-#clean our workspace
+#-- Analysis of forecasts --#
+
+
+# begin by clearing our workspace
 rm(list=ls())
 
+#set our current working directory
+setwd("/home/andrew")
+setwd("/home/andrew/Documents/school/845/project/ACSTS845-project")
 
-# -- Load in required libraries -- #
-library(quantmod) #used to fetch the data
-library(DistributionUtils) # for the kurtosis function
-
-#-- load in data --#
-
-equity.tickers<- c("MSFT","AAPL","BAC")
-currency.tickers<- c("CAD=X","JPY=X", "GBP=X")
-commodity.tickers <- c("COM-AU_EFP","COM-CHEESE_BRL","COM-SOYB_MEAL")
+#-- load require libraries --#
+library(rugarch)
 
 
-getSymbols(Symbols=equity.tickers,
-           from="2000-01-01",
-           to="2016-12-01",
-           src="yahoo")
+# load in our previously computed forecast
+forecasts.list<- readRDS("student-CAD=X.rds")
 
-getSymbols(Symbols=currency.tickers,
-           from="2004-01-01",
-           to="2016-12-01",
-           src="yahoo")
+#initialize variables
+alpha.levels <- c(0.005,0.01,0.05,0.10)
 
 
-commodity.returns <- list(rep(NA,3))
+#violations <- forecasts.list[[1]]@forecast$VaR >forecasts.list[[1]]@forecast$VaR[,"realized"]
+#colSums(violations)/ dim(violations)[1]
 
-for (i in 1:3){
-  price.df =read.csv(paste0(commodity.tickers[i], ".csv"))
-  prices   =xts(price.df$Value, order.by = as.POSIXct( price.df$Date)  )
-  commodity.returns[[i]] <- dailyReturn(prices,type="log")
-}
-
-
-summary.stats <- function(returns.series, name="series"){
-  r.n= length(returns.series)
-  r.mean= mean(returns.series)
-  r.sd= sd(returns.series)
-  r.min= min(returns.series)
-  r.max= max(returns.series)
-  r.kurt= kurtosis(returns.series)
-  r.skew =skewness(returns.series)
-  print(paste(name, "summary statistics:"))
-  #print("r.n,r.mean,r.sd,r.min,r.max,r.kurt,r.skew")
-  #using a tab lets you paste it into spreadsheet programs!
-  print(cat(c(r.n,r.mean,r.sd,r.min,r.max,r.kurt,r.skew),sep ="\t"))
+#Note 1: ARCH(1), 2:GARCH(1,1) , 3:ARMAGARCH , 4: EGARCH
+for (i in 1:(length(alpha.levels))){
+  report(forecasts.list[[]], type="VaR", VaR.alpha = alpha.levels[i], conf.level = 0.95) 
 }
 
 
 
-tickers<-c(equity.tickers,commodity.tickers,currency.tickers)
-returns <- list( dailyReturn(MSFT$MSFT.Adjusted,type="log"),
-                 dailyReturn(AAPL$AAPL.Adjusted,type="log"),
-                 dailyReturn(BAC$BAC.Adjusted,type="log"),
-                 commodity.returns[[1]],
-                 commodity.returns[[2]],
-                 commodity.returns[[3]],
-                 dailyReturn(`CAD=X`$`CAD=X.Adjusted`,type="log"),
-                 dailyReturn(`JPY=X`$`JPY=X.Adjusted`,type="log"),
-                 dailyReturn(`GBP=X`$`GBP=X.Adjusted`,type="log")
-)
 
-for (i in 1:(length(tickers))){
-  summary.stats(returns.series=returns[[i]],name=tickers[i])
+report(forecasts.list[[4]], type="VaR", VaR.alpha = alpha.levels[1], conf.level = 0.95) 
+forecasts.list[[1]]<-resume(forecasts.list[[1]], solver="lbfgs")
+forecasts.list[[2]]<-resume(forecasts.list[[2]], solver="lbfgs")
+forecasts.list[[3]]<-resume(forecasts.list[[3]], solver="lbfgs")
+forecasts.list[[4]]<-resume(forecasts.list[[4]], solver="lbfgs")
+
+forecasts.list[[1]]<-resume(forecasts.list[[1]], solver="hybrid",solver.control=list(rel.tol=1e-4,delta=1e-5))
+forecasts.list[[2]]<-resume(forecasts.list[[2]], solver="hybrid",solver.control=list(rel.tol=1e-4,delta=1e-5))
+forecasts.list[[3]]<-resume(forecasts.list[[3]], solver="hybrid",solver.control=list(rel.tol=1e-4,delta=1e-5))
+forecasts.list[[4]]<-resume(forecasts.list[[4]], solver="hybrid",solver.control=list(rel.tol=1e-4,delta=1e-5))
+
+forecasts.list[[3]]<-resume(forecasts.list[[3]], solver="hybrid",solver.control=list(rel.tol=1e-4,delta=1e-5,trace =TRUE),fit.control = list(stationarity=FALSE, fixed.se = 0, scale=0))
+forecasts.list[[3]]<-resume(forecasts.list[[3]], solver="hybrid",solver.control=list(rel.tol=1e-3,delta=1e-5,trace =TRUE),fit.control = list(stationarity=FALSE, fixed.se = 0, scale=0))
+
+
+for (i in 1:4){
+  #lol this could be done with the convergence() function apparently
+  if(!is.null(forecasts.list[[i]]@forecast[[1]]$converge)){
+    print(paste(sum(sapply(1:(length(forecasts.list[[i]]@forecast)),function (x) !forecasts.list[[i]]@forecast[[x]]$converge)), "window(s) do(es) not converge!"))
+  }
+  else{
+    print("all windows converge")
+  }
 }
+test <- forecasts.list[[2]]
+test <- forecasts.list[[2]]@forecast[setdiff(1:(length(forecasts.list[[2]]@forecast)) ,attributes(convergence(forecasts.list[[2]]))$nonconverged)]
 
+forecasts.list[[2]]@forecast$VaR
+test
+
+test@model$noncidx <-as.vector(NULL)
+test@forecast[[2981]]
+report(test, type="VaR", VaR.alpha = alpha.levels[i], conf.level = 0.95)
+convergence(test)
+typeof()
+as.vector(convergence(forecasts.list[[2]]))
+attributes(convergence(forecasts.list[[2]]))$nonconverged
+sum(sapply(1:3,function (x) not(forecasts.list[[1]]@forecast[[x]]$converge)))
+summary(forecasts.list[[3]]@forecast[1:4])
+
+!is.null(forecasts.list[[1]]@forecast[[1]]$converge)
+!is.null(forecasts.list[[4]]@forecast[[1]]$converge)
+typeof(forecasts.list[[4]]@forecast[[1]])
