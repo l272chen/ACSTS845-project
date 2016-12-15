e7cf24e002ecd356f9fdf28496a142e0d1e6aeb6
Andrew <vandenhoeven.andrew@gmail.com>
2016-12-15 12:24:31 -0500

    reverted to save some changes that I almost lost

--- 845Ranalysis.R
+++ 845Ranalysis.R
@@ -1,34 +1,73 @@
-#    845 Project
+# 845  project 
 #
 #   Leiguang Chen
 # Andrew van den Hoeven
+# summary stats
 
-#-- Analysis of forecasts --#
-
-
-# begin by clearing our workspace
+#clean our workspace
 rm(list=ls())
 
-#set our current working directory
-#setwd("/home/andrew")
 
-#-- load require libraries --#
-library(rugarch)
+# -- Load in required libraries -- #
+library(quantmod) #used to fetch the data
+library(DistributionUtils) # for the kurtosis function
 
+#-- load in data --#
 
-# load in our previously computed forecast
-forecasts.list<- readRDS("normal-MSFT.rds")
-
-#initialize variables
-alpha.levels <- c(0.005,0.01,0.05,0.10)
+equity.tickers<- c("MSFT","AAPL","BAC")
+currency.tickers<- c("CAD=X","JPY=X", "GBP=X")
+commodity.tickers <- c("COM-AU_EFP","COM-CHEESE_BRL","COM-SOYB_MEAL")
 
 
-violations <- forecasts.list[[3]]@forecast$VaR >forecasts.list[[3]]@forecast$VaR[,"realized"]
-colSums(violations)/ dim(violations)[1]
+getSymbols(Symbols=equity.tickers,
+           from="2000-01-01",
+           to="2016-12-01",
+           src="yahoo")
+
+getSymbols(Symbols=currency.tickers,
+           from="2004-01-01",
+           to="2016-12-01",
+           src="yahoo")
 
 
-for (i in 1:(length(alpha.levels))){
-  report(forecasts.list[[4]], type="VaR", VaR.alpha = alpha.levels[i], conf.level = 0.95) 
+commodity.returns <- list(rep(NA,3))
+
+for (i in 1:3){
+  price.df =read.csv(paste0(commodity.tickers[i], ".csv"))
+  prices   =xts(price.df$Value, order.by = as.POSIXct( price.df$Date)  )
+  commodity.returns[[i]] <- dailyReturn(prices,type="log")
 }
 
-forecasts.list[[4]]@model$coef
+
+summary.stats <- function(returns.series, name="series"){
+  r.n= length(returns.series)
+  r.mean= mean(returns.series)
+  r.sd= sd(returns.series)
+  r.min= min(returns.series)
+  r.max= max(returns.series)
+  r.kurt= kurtosis(returns.series)
+  r.skew =skewness(returns.series)
+  print(paste(name, "summary statistics:"))
+  #print("r.n,r.mean,r.sd,r.min,r.max,r.kurt,r.skew")
+  #using a tab lets you paste it into spreadsheet programs!
+  print(cat(c(r.n,r.mean,r.sd,r.min,r.max,r.kurt,r.skew),sep ="\t"))
+}
+
+
+
+tickers<-c(equity.tickers,commodity.tickers,currency.tickers)
+returns <- list( dailyReturn(MSFT$MSFT.Adjusted,type="log"),
+                 dailyReturn(AAPL$AAPL.Adjusted,type="log"),
+                 dailyReturn(BAC$BAC.Adjusted,type="log"),
+                 commodity.returns[[1]],
+                 commodity.returns[[2]],
+                 commodity.returns[[3]],
+                 dailyReturn(`CAD=X`$`CAD=X.Adjusted`,type="log"),
+                 dailyReturn(`JPY=X`$`JPY=X.Adjusted`,type="log"),
+                 dailyReturn(`GBP=X`$`GBP=X.Adjusted`,type="log")
+)
+
+for (i in 1:(length(tickers))){
+  summary.stats(returns.series=returns[[i]],name=tickers[i])
+}
+
--- 845rscript.R
+++ 845rscript.R
@@ -6,7 +6,7 @@
 
 
 # begin by clearing our workspace
-rm(list=ls()) 
+rm(list=ls())
 
 
 # -- Load in required libraries -- #
@@ -15,15 +15,16 @@ library(rugarch)
 library(parallel) #used to run the code in parallel
 
 # -- Load in the data -- #
-# for now we'll just use a single set for testing out the r code
+setwd("/home/andrew/Documents/school/845/project/ACSTS845-project")
 
-ticker<- "JPY=X"
-prices<-getSymbols(Symbols=c(ticker),
-        from="2004-01-01",
-        to="2016-12-01",
-        src="yahoo",
-        env=NULL)[,6]
-
+ticker<- "COM-SOYB_MEAL"
+#prices<-getSymbols(Symbols=c(ticker),
+#        from="2004-01-01",
+#        to="2016-12-01",
+#        src="yahoo",
+#        env=NULL)[,6]
+price.df <-read.csv(paste0(ticker, ".csv"))
+prices <- xts(price.df$Value, order.by = as.POSIXct( price.df$Date)  )
 
 # Also we're concerned with the log returns of the adjusted close
 returns <- dailyReturn(prices,type="log")
@@ -37,16 +38,16 @@ alpha.levels <- c(0.005,0.01,0.05,0.10)
 
 
 models.norm <- list( ugarchspec(variance.model = list(garchOrder=c(1,0)), mean.model = list(armaOrder=c(0,0))) , # ARCH(1)
-                ugarchspec(variance.model = list(garchOrder=c(1,1)), mean.model = list(armaOrder=c(0,0))) , #GARCH(1,1)
-                ugarchspec(variance.model = list(garchOrder=c(1,1)), mean.model = list(armaOrder=c(1,1))) , #ARMA(1,1)-GARCH(1,1)
-                ugarchspec(variance.model = list(model = "eGARCH", garchOrder=c(1,1)), mean.model = list(armaOrder=c(0,0)))  #EGARCH(1,1)
-                )
+                     ugarchspec(variance.model = list(garchOrder=c(1,1)), mean.model = list(armaOrder=c(0,0))) , #GARCH(1,1)
+                     ugarchspec(variance.model = list(garchOrder=c(1,1)), mean.model = list(armaOrder=c(1,1))) , #ARMA(1,1)-GARCH(1,1)
+                     ugarchspec(variance.model = list(model = "eGARCH", garchOrder=c(1,1)), mean.model = list(armaOrder=c(0,0)))  #EGARCH(1,1)
+)
 forecasts.list.norm <- as.list(rep(NA,4))
 
 models.student <- list( ugarchspec(variance.model = list(garchOrder=c(1,0)), mean.model = list(armaOrder=c(0,0)),distribution.model="std") , # ARCH(1)
-                     ugarchspec(variance.model = list(garchOrder=c(1,1)), mean.model = list(armaOrder=c(0,0)),distribution.model="std") , #GARCH(1,1)
-                     ugarchspec(variance.model = list(garchOrder=c(1,1)), mean.model = list(armaOrder=c(1,1)),distribution.model="std") , #ARMA(1,1)-GARCH(1,1)
-                     ugarchspec(variance.model = list(model = "eGARCH", garchOrder=c(1,1)), mean.model = list(armaOrder=c(0,0)),distribution.model="std")  #EGARCH(1,1)
+                        ugarchspec(variance.model = list(garchOrder=c(1,1)), mean.model = list(armaOrder=c(0,0)),distribution.model="std") , #GARCH(1,1)
+                        ugarchspec(variance.model = list(garchOrder=c(1,1)), mean.model = list(armaOrder=c(1,1)),distribution.model="std") , #ARMA(1,1)-GARCH(1,1)
+                        ugarchspec(variance.model = list(model = "eGARCH", garchOrder=c(1,1)), mean.model = list(armaOrder=c(0,0)),distribution.model="std")  #EGARCH(1,1)
 )
 forecasts.list.student <- as.list(rep(NA,4))
 
@@ -61,17 +62,17 @@ cl <- makeCluster(no_cores)
 
 for( i in 1:(length(models.norm))){
   forecasts.list.norm[[i]]<- ugarchroll(spec=models.norm[[i]],data=returns,n.ahead = 1,
-                                refit.every = 1, refit.window ="moving",n.start =window.size,
-                                window.size=window.size,VaR.alpha = alpha.levels,
-                                cluster=cl)
+                                        refit.every = 1, refit.window ="moving",n.start =window.size,
+                                        window.size=window.size,VaR.alpha = alpha.levels,
+                                        cluster=cl)
   print(paste("Completed normal forecast for model", i))
 }
 
 for( i in 1:(length(models.student))){
   forecasts.list.student[[i]]<- ugarchroll(spec=models.student[[i]],data=returns,n.ahead = 1,
-                                        refit.every = 1, refit.window ="moving",n.start =window.size,
-                                        window.size=window.size,VaR.alpha = alpha.levels,
-                                        cluster=cl)
+                                           refit.every = 1, refit.window ="moving",n.start =window.size,
+                                           window.size=window.size,VaR.alpha = alpha.levels,
+                                           cluster=cl)
   print(paste("Completed student's t forecast for model", i))
 }
 
